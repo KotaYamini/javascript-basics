8124c129f75c9f741b2fbda0abfe0a1fa0d0e463
yaminikota <yamini.kota466@gmail.com>
2023-04-16 18:59:26 +0530

    prototypeUsecases

---
+++ prototypeUseCases.js
@@ -0,0 +1,7 @@
+/**
+ * Prototype is one of the most important concept in javascript. Every object in javascript inherits from Object.prototype. 
+
+There are basically 2 use case of prototype:
+  1) adding properties and methods on Prototype object
+  2) implement inheritance
+ */
