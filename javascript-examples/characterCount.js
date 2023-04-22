75b12575ccf284c2ea3d1018a5ec0437941a95dd
yaminikota <yamini.kota466@gmail.com>
2023-04-22 16:19:05 +0530

    cosmectic

--- javascript-examples/characterCount.js
+++ javascript-examples/characterCount.js
@@ -2,6 +2,7 @@ function getFrequency(string) {
     var freq = {};
     for (let i = 0; i < string.length; i++) {
         let character = string.charAt(i);
+        console.log(character) // Here we will get all characters aone by one as we are fecthing letter at that particular index
         if (freq[character]) {
             freq[character]++;
         } else {
