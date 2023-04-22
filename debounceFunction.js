91f785865640fce9a8fb94bb25223bdd9ede99cb
yaminikota <yamini.kota466@gmail.com>
2023-04-22 23:26:38 +0530

    debounce

--- debounceFunction.js
+++ debounceFunction.js
@@ -7,19 +7,19 @@
 const ele = document.querySelector("input");
 
 
-function outcome() {
-    console.log('data received from server');
+function outcome(data) {
+    console.log('data received from server', data);
 }
 
 //with debounce
-function hofDebounce(func, timeout) {
-    let timer;
-    return function () {
+function hofDebounce(func, timeout) { // using the concept of Higher Order Functions
+    let timer; // using the concept of scope 
+    return function (...args) {
         if (timer)
-            clearTimeout(timer); // Here we are cancelling the unnecessary calls before the defined timeout called 1000 seconds
+            clearTimeout(timer); // Here we are cancelling the unnecessary calls called cpu cycles & api calls before the defined timeout called 1000 seconds
         setTimeout(() => {
-            func();
-        }, timeout);
+            func(args); // using the concept of closures
+        }, timeout); // we are delaying the every keyup by using setTimeout (i.e, async programming)
     }
 }
 
@@ -27,4 +27,4 @@ function hofDebounce(func, timeout) {
 const result = hofDebounce(outcome, 1000);
 console.log(result);
 
-ele.addEventListener('keyup', result);
+ele.addEventListener("keyup", e => result(e.target.value));
