8981dad324db847c943d4721365151484d88fe7f
yaminikota <yamini.kota466@gmail.com>
2023-07-17 20:07:09 +0530

    changes

--- Prototypical-Inheritance.js
+++ Prototypical-Inheritance.js
@@ -26,3 +26,34 @@ Samsung.prototype = new Phone();
 let obj = new Samsung();
 console.log(obj.getModelNumber()); //XXX-YYYY-ZZZ Here Samsung object will search for getModelNumber method,  as we register the phone object to the samsung prototype it will get the instance of the method
 obj.latestFeature();
+
+
+/**
+ * In prototypical inheritance, each object has an internal link to another object called its prototype. When a property or method is accessed on an object, and the object doesn't have that property or method, the JavaScript engine looks up the prototype chain to find it.
+ */
+
+
+// Constructor function or prototype object
+function Animal(name) {
+    this.name = name;
+  }
+  
+  // Method on the prototype
+  Animal.prototype.sayName = function() {
+    console.log("My name is " + this.name);
+  };
+  
+  // Create a new object using the prototype
+  var cat = new Animal("Whiskers");
+  
+  // Access property and method
+  console.log(cat.name);  // Output: Whiskers
+  cat.sayName();          // Output: My name is Whiskers
+//   In this example, the Animal function serves as the prototype for the cat object. The cat object inherits the name property and the sayName method from its prototype.
+  
+//   Prototypical inheritance allows for efficient sharing of properties and methods among objects and is a key feature of prototype-based programming languages like JavaScript.
+  
+  
+  
+  
+  
